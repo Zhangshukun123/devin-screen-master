9e7038fa16b525814e066f48c6ef6605305b8db0
3897352339@qq.com <3897352339@qq.com>
2025-08-28 14:33:44 +0800

    设置页面调整中

--- app/src/main/java/com/diwen/liliao/activity/DeviceSettingActivity.java
+++ app/src/main/java/com/diwen/liliao/activity/DeviceSettingActivity.java
@@ -30,6 +30,14 @@ public class DeviceSettingActivity extends MqttBaseActivity<LayoutDevicesettinga
 
     @Override
     protected void setUiText() {
+        binding.tvBluetooth.setText(DemoApp.getInstance().getAppViewModel().getLangText("设置蓝牙"));
+        binding.tvWifi.setText(DemoApp.getInstance().getAppViewModel().getLangText("设置WiFi"));
+        binding.tvTime.setText(DemoApp.getInstance().getAppViewModel().getLangText("设置时间"));
+        binding.tvLanguage.setText(DemoApp.getInstance().getAppViewModel().getLangText("设置语言"));
+        binding.tvFans.setText(DemoApp.getInstance().getAppViewModel().getLangText("设置风扇"));
+        binding.tvVersion.setText(DemoApp.getInstance().getAppViewModel().getLangText("设置版本"));
+        binding.llBlueTooth.setBackgroundResource(R.drawable.bg_radius_4_white10);
+        binding.ivBluetooth.setImageResource(R.mipmap.ic_bluetooth_setting_check);
         settingItems = new ArrayList<>();
         settingItems.add(new SettingItem(R.mipmap.icon_languge, "语言设置"));
         settingItems.add(new SettingItem(R.mipmap.icon_air, "风扇设置"));
@@ -48,9 +56,9 @@ public class DeviceSettingActivity extends MqttBaseActivity<LayoutDevicesettinga
                     ActivityUtils.startActivity(AirSettingActivity.class);
                     break;
                 case 2:
-                    if (DemoApp.getInstance().buildCompany){
+                    if (DemoApp.getInstance().buildCompany) {
                         ActivityUtils.startActivity(MaiChongSettingActivity.class);
-                    }else {
+                    } else {
                         ActivityUtils.startActivity(DeviceModelActivity.class);
                     }
                     break;
@@ -61,7 +69,7 @@ public class DeviceSettingActivity extends MqttBaseActivity<LayoutDevicesettinga
                     ActivityUtils.startActivity(UseHitorActivity.class);
                     break;
                 case 5:
-                  ActivityUtils.startActivity(DeviceListActivity.class);
+                    ActivityUtils.startActivity(DeviceListActivity.class);
                     break;
             }
 
@@ -76,6 +84,12 @@ public class DeviceSettingActivity extends MqttBaseActivity<LayoutDevicesettinga
     @Override
     protected void setListener() {
         binding.ivFinish.setOnClickListener(this);
+        binding.llBlueTooth.setOnClickListener(this);
+        binding.llWifi.setOnClickListener(this);
+        binding.llTime.setOnClickListener(this);
+        binding.llLanguage.setOnClickListener(this);
+        binding.llFans.setOnClickListener(this);
+        binding.llVersion.setOnClickListener(this);
 
     }
 
@@ -86,5 +100,50 @@ public class DeviceSettingActivity extends MqttBaseActivity<LayoutDevicesettinga
             BTCodeUtils.getInstance().finishTo(1);
             ActivityUtils.finishActivity(DeviceSettingActivity.class);
         }
+        if (v == binding.llBlueTooth) {
+            resetCheckView();
+            binding.llBlueTooth.setBackgroundResource(R.drawable.bg_radius_4_white10);
+            binding.ivBluetooth.setImageResource(R.mipmap.ic_bluetooth_setting_check);
+        }
+        if (v == binding.llWifi) {
+            resetCheckView();
+            binding.llWifi.setBackgroundResource(R.drawable.bg_radius_4_white10);
+            binding.ivWifi.setImageResource(R.mipmap.ic_wifi_setting_check);
+        }
+        if (v == binding.llTime) {
+            resetCheckView();
+            binding.llTime.setBackgroundResource(R.drawable.bg_radius_4_white10);
+            binding.ivTime.setImageResource(R.mipmap.ic_time_setting_check);
+        }
+        if (v == binding.llLanguage) {
+            resetCheckView();
+            binding.llLanguage.setBackgroundResource(R.drawable.bg_radius_4_white10);
+            binding.ivLanguage.setImageResource(R.mipmap.ic_language_setting_check);
+        }
+        if (v == binding.llFans) {
+            resetCheckView();
+            binding.llFans.setBackgroundResource(R.drawable.bg_radius_4_white10);
+            binding.ivFans.setImageResource(R.mipmap.ic_fans_setting_check);
+        }
+        if (v == binding.llVersion) {
+            resetCheckView();
+            binding.llVersion.setBackgroundResource(R.drawable.bg_radius_4_white10);
+            binding.ivVersion.setImageResource(R.mipmap.ic_version_setting_check);
+        }
+    }
+
+    private void resetCheckView() {
+        binding.llBlueTooth.setBackgroundResource(R.drawable.bg_translate);
+        binding.ivBluetooth.setImageResource(R.mipmap.ic_bluetooth_setting);
+        binding.llWifi.setBackgroundResource(R.drawable.bg_translate);
+        binding.ivWifi.setImageResource(R.mipmap.ic_wifi_setting);
+        binding.llTime.setBackgroundResource(R.drawable.bg_translate);
+        binding.ivTime.setImageResource(R.mipmap.ic_time_setting);
+        binding.llLanguage.setBackgroundResource(R.drawable.bg_translate);
+        binding.ivLanguage.setImageResource(R.mipmap.ic_language_setting);
+        binding.llFans.setBackgroundResource(R.drawable.bg_translate);
+        binding.ivFans.setImageResource(R.mipmap.ic_fans_setting);
+        binding.llVersion.setBackgroundResource(R.drawable.bg_translate);
+        binding.ivVersion.setImageResource(R.mipmap.ic_version_setting);
     }
 } 
