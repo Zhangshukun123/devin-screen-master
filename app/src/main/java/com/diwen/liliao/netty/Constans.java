755fd667a662c35961c4443ce0d7a03c6e9be348
3897352339@qq.com <3897352339@qq.com>
2025-10-10 16:03:57 +0800

    端口号更改，避免连上老设备

--- app/src/main/java/com/diwen/liliao/netty/Constans.java
+++ app/src/main/java/com/diwen/liliao/netty/Constans.java
@@ -3,7 +3,7 @@ package com.diwen.liliao.netty;
 public class Constans {
 
    public static final String HOST = "39.106.22.144";
-    public static final int TCP_PORT = 1026;
+    public static final int TCP_PORT = 1027;
     public static final String aeskey = "aFGN6ieL005^AWrs";//必须16位
     public static final String S_END = "\r\n";
     public static final String sid = "?sid";
